a7e9d9bb515d61c104ee76ee694f5078139365b1
alexsandrbarabash <sbarabas176@gmail.com>
2021-01-22 01:10:53 +0200

    fixed bug on server script

--- server/app.js
+++ server/app.js
@@ -7,14 +7,16 @@ const app = express();
 app.use(favicon(__dirname + "../build/favicon.ico"));
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "../build")));
+
 
 app.use("/sort", require('./routers/sort'));
 app.use("/data", require('./routers/getData'));
 
-app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../build")));
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "build", "index.html"));
+    });
+}
 module.exports = app;
 
