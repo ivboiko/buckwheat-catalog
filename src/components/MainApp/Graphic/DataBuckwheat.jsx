7be66316b29cec1cb9381b53e08c567e8f4573b3
alexsandrbarabash <sbarabas176@gmail.com>
2021-01-22 02:15:50 +0200

    Added recharts

---
+++ src/components/MainApp/Graphic/DataBuckwheat.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DataBuckwheat.scss';
+
+
+
+const DataBuckwheat = () => {
+    return (
+        <>
+            Тут повині бути дані
+        </>
+    );
+};
+
+export default DataBuckwheat;
--- src/components/MainApp/Graphic/Graphic.js
+++ src/components/MainApp/Graphic/Graphic.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Graphic.scss';
+import GraphicBuckwheat from "./GraphicBuckwheat";
 
 const Graphic = () => {
   return (
     <div className="graphic app-child">
-
+      <GraphicBuckwheat/>
     </div>
   );
 };
