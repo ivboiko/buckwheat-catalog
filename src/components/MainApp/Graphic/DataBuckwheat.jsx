36d80a64986a58be426f2be62f8341e7b3786743
Artur24327 <57686461+Artur24327@users.noreply.github.com>
2021-01-22 22:49:29 +0200

    DateBuckwheat
    DateBuckwheat

--- src/components/MainApp/Graphic/DataBuckwheat.jsx
+++ src/components/MainApp/Graphic/DataBuckwheat.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './DataBuckwheat.scss';
-
+import arrowDown from '../../../assets/imgs/Arrow_Down.png';
 
 const DataBuckwheat = () => {
     return (
-        <div id="DataBlock">
-            <a>NICE</a>
+        <div className="DataBlock">
+
+            <div className="NameBrand">VARUS</div>
+            
+            <div className="description" >Крупа гречана ядриця швидкорозварювана</div>
+
+            <div className="GoodsNumbers">
+
+                <div className="DataPrice">₴42.99</div>
+
+                <div className="persent">-12%<img className="arrow" src={arrowDown}/></div>
+
+            </div>
+
         </div>
     );
 };
