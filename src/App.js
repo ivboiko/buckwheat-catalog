936842501dc4ac85c4fe09cde01bdc22769d7968
Ivan <ivboikovst@gmail.com>
2021-01-22 02:06:06 +0200

    added cards section to main block

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import MainApp from './components/MainApp/MainApp';
 const App = () => (
   <>
     <Header />
-    {/*<Card*/}
-    {/*  number={1}*/}
-    {/*  logoSrc={novus}*/}
-    {/*  storeName='Novus'*/}
-    {/*  productName='Крупа гречана Novus ядриця швидкорозварювана'*/}
-    {/*  weight='400 г'*/}
-    {/*  price={22.99}*/}
-    {/*  bestPrice />*/}
     <MainApp />
   </>
 );
