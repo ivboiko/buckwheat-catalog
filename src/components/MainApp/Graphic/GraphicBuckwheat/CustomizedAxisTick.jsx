08e8d17c0638b9b3f2833de94da6321c6d5bd45f
alexsandrbarabash <sbarabas176@gmail.com>
2021-01-22 19:53:09 +0200

    Refactoring

--- src/components/MainApp/Graphic/GraphicBuckwheat/CustomizedAxisTick.jsx
+++ src/components/MainApp/Graphic/GraphicBuckwheat/CustomizedAxisTick.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
-const CustomizedAxisTick = ({ x, y, stroke, payload, axis }) => {
-  console.log("x", x);
-  console.log("y", y);
-  console.log("payload", payload);
+const CustomizedAxisTick = ({ x, y, payload, axis }) => {
 
   return (
     <g transform={`translate(${x},${y})`}>
       <text
-        x={axis === "x" ? 20 : 0}
+        x={axis === "x" ? 8 : 0}
         y={axis === "x" ? 0 : 4}
         dy={axis === "x"? 16: 0}
         fontFamily="Inter-Bold, serif"
