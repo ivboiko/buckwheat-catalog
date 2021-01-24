c339ef411c5d1d268ac9e26cb265f230f6b9d76f
Ivan <ivboikovst@gmail.com>
2021-01-25 01:32:14 +0200

    added plug screen

--- src/components/MainApp/Graphic/Graphic.js
+++ src/components/MainApp/Graphic/Graphic.js
@@ -4,6 +4,7 @@ import GraphicBuckwheat from './GraphicBuckwheat';
 import DataBuckwheat from './DataBuckwheat';
 import classNames from 'classnames';
 import {theme} from '../../../redux/reducers/app-reducer';
+import GraphicPlug from './GraphicPlug/GraphicPlug';
 
 const Graphic = ({appTheme}) => {
   const graphicClassName = classNames(
@@ -16,15 +17,16 @@ const Graphic = ({appTheme}) => {
 
 
   return (
-    <div className={graphicClassName}>
-      <GraphicBuckwheat appTheme={appTheme} />
-      <DataBuckwheat
-        storeName="VARUS"
-        itemName="Крупа гречана ядриця швидкорозварювана"
-        price={42.99}
-        percent={-12}
-        appTheme={appTheme} />
-    </div>
+    // <div className={graphicClassName}>
+    //   <GraphicBuckwheat appTheme={appTheme} />
+    //   <DataBuckwheat
+    //     storeName="VARUS"
+    //     itemName="Крупа гречана ядриця швидкорозварювана"
+    //     price={42.99}
+    //     percent={-12}
+    //     appTheme={appTheme} />
+    // </div>
+    <GraphicPlug appTheme={appTheme} />
   );
 };
 
