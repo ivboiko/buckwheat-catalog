85906e20d3310beee2dcae6cb5db25e9e55e1e20
Ivan <ivboikovst@gmail.com>
2021-01-25 22:26:16 +0200

    sort bug fix

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,6 +1,7 @@
 export const sortByPrice = (isAscending, sortBuy) => (item1, item2) => {
-  const price1 = Number(item1[sortBuy]).toFixed(2);
-  const price2 = Number(item2[sortBuy]).toFixed(2);
+  const price1 = Number(item1[sortBuy].toFixed(2));
+  const price2 = Number(item2[sortBuy].toFixed(2));
+
   // debugger
 
   if (price1 === price2) {
